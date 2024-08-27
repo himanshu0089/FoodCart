f54b67097f9ca0372b3b3ed3a2a0d393eb12843b
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-08-27 23:14:10 +0530

    feat:Updating cart items

--- vite-project/src/components/cartItems.jsx
+++ vite-project/src/components/cartItems.jsx
@@ -2,25 +2,55 @@ import React, { useEffect } from "react";
 import { useCart } from "./cart-context";
 import { menuList } from "./menuList";
 
-function CartItems() {
-    const {itemRef,RemoveFromCart}=useCart();
-     useEffect(()=>{
-        console.log({menuList})
-     },[])
-    return (
+function CartItems({ setShowCart , setItemCounter}) {
+  const { itemRef, RemoveFromCart, addToCart,totalCartAmount } = useCart();
+  const addToCarthandler=(id)=>{
+    addToCart(id);
+    setItemCounter(prev=>prev+1)
+  }
+  const RemoveFromCarthandler=(id)=>{
+    RemoveFromCart(id);
+    setItemCounter(prev=>prev-1)
+  }
+  useEffect(() => {
+    console.log({ menuList });
+  }, []);
+  return (
     <div>
+      <div className="bg-gray-400 z-10">
         <div className="">
-            <div className="">
-                {menuList.map((item,index)=>{
-                    return (
-                        <div key={index}> {item.name} </div>
-                    )
-                })}
-            </div>
-            <button onClick={()=>RemoveFromCart(menuList.id)}>
-                -
-            </button>
+          {menuList.map((item, index) => {
+            if (itemRef[item.id] > 0) {
+              return (
+                <div key={index} className="flex flex-col">
+                  <div className="">
+                  <p className=""> {item.name}</p>
+                  <p className="">Rs. {item.price} </p>
+                  <p className=""> x {itemRef[item.id]}</p>
+                  </div>
+                  
+                  <div className="flex">
+                  <p className="cursor-pointer" onClick={() => addToCarthandler(item.id)}> +</p>
+                  <p className="cursor-pointer" onClick={() => RemoveFromCarthandler(item.id)}>-</p>
+                  </div>
+                  
+                  <hr />
+                </div>
+              );
+            }
+          })}
         </div>
+        <div className="flex justify-between">
+        <p>CartAmount : </p>
+        <p> Rs. {totalCartAmount()}</p>
+        </div>
+        <div className="flex flex-row-reverse">
+        
+        <button onClick={() => setShowCart(false)}> Close</button>
+        
+        <button>orders</button> 
+        </div>
+      </div>
     </div>
   );
 }
