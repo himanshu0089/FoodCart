5fb0c3a037e4d9a8394ec18274ad731323ebdf57
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-08-30 17:17:58 +0530

    refractor: Added image files

--- vite-project/src/components/Container.jsx
+++ vite-project/src/components/Container.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+// import food from "../assets/Food" 
 function Container() {
   return (
     <div className="bg-gray-300 text-green-600 text-center mx-20 my-5 py-5">
       {" "}
+      {/* <img src="food.jpg" alt="" />  */}
       <h2 className="font-bold"> Delicious Food, Delivered To You </h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
